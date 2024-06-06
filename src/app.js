0f65cf5b24129cf8a30466dba674b00d6a3ef500
Abhisheklogics <abhi.kumar252004@gmail.com>
2024-06-06 14:40:17 +0530

    app filr changes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 
 app.use(cors({
-    origin:'https://jazzy-conkies-0dfd4d.netlify.app/ ',
+    origin:'*',
     credentials:true
 }
     
