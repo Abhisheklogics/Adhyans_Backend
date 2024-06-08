316d2de511715450b2e13e4c642cd13b47178e30
Abhisheklogics <abhi.kumar252004@gmail.com>
2024-06-08 12:59:19 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 
 app.use(cors({
-    origin:'https://frontend-al2m.onrender.com',
+    origin:'https://frontend-l645.onrender.com',
     credentials:true
 }
     
