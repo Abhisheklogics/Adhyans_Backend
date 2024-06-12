fd45715e52dd3f55351b3ecaef48286d80ace554
Abhisheklogics <abhi.kumar252004@gmail.com>
2024-06-12 12:46:47 +0530

    routes comment

--- src/routes/ar.routes.js
+++ src/routes/ar.routes.js
@@ -30,7 +30,7 @@ router.route('/homeImagesSave').post(upload.fields([
     
     
 ]),homeImages)
-router.route('/homeData').get(getHomeImages)
+router.route('/').get(getHomeImages)
 router.route("/datasave").post(upload.fields([
     {
         name:'image1',
